979b41c816e816bae6d9acf16e877c7efed483ef
bjwine0 <bradsmilek@gmail.com>
2019-02-02 11:36:59 -0800

    lab 2 js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,45 @@
 'use strict';
 
-var dogs = prompt('How many dogs do I own?');
+var userName = prompt('Hi, what is your name?');
+console.log('userName');
+alert('Nice to meet you ' + userName);
 
-console.log('Number of dogs, ', dogs);
+var ready = prompt('I have made a silly game of questions for you to get to know me a bit better! Boy, it is your lucky day! Are you ready to play '
+ + userName + '?');
+console.log('ready');
+if (ready.toLowerCase() === 'yes' || ready.toLowerCase() === 'y') {
+  alert('Let us begin.');
+} else {
+  alert('Aw cmon, no party poopers allowed!');
+}
 
-alert('Number of dogs, ' + dogs);
+var cake = prompt('Do I like cake ' + userName + '?');
+console.log('cake');
+if (cake.toLowerCase() === 'yes' || cake.toLowerCase() === 'y') {
+  alert('I actually prefer pie');
+} else {
+  alert('How did you know? I do love pie!');
+}
 
-var guitars = prompt('How many guitars do you have?');
+var pets = prompt('Do I have any pets?');
+console.log('pets');
+if (pets.toLowerCase() === 'no' || pets.toLowerCase() === 'n') {
+  alert('Correct, but I do love animals!');
+} else {
+  alert('No I do not, my last dog named Blue passed away and I am still sad (sad face)');
+}
+var nature = prompt('Could I survive the outdoors?');
+console.log('nature');
+if (nature.toLowerCase() === 'no' || nature.toLowerCase() === 'n') {
+  alert('What?! Actually I can, I have taken several survival classes and hike into the "nothing" every chance I get.');
+} else {
+  alert('Like a pro, I have trained my whole life in tactical survival within several environments.');
+}
 
-if (guitars === '1' || guitars === '2') {
-  alert('Yes I have ' + guitars + ' guitars!');
+var passion = prompt('Is having passion for what I do important to me?');
+console.log('passion');
+if (passion.toLowerCase() === 'yes' || passion.toLowerCase() === 'y') {
+  alert('Yes, very important to me. It provides the reason behind my ambition and happiness.');
+} else {
+  alert('Of course it is, passion provides the reason behind my ambition and happiness.');
 }
